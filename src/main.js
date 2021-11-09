d80b276258c2e1f889672396d14a47381c5cc871
Isabel <isabeltellohuaripata@gmail.com>
2021-11-08 20:47:56 -0500

    arreglos

--- src/main.js
+++ src/main.js
@@ -26,11 +26,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
     let i=1;
     const img1 = document.querySelector("#img1");
     const img2 = document.querySelector("#img2");
-    const progresssBar = document.querySelector('#progress-bar');
     const divIndicadores = document.querySelector('#indicadores');
-    let porcentaje_base = 100/imagenes.length;
-    let porcentaje_actual = porcentaje_base;
-
     for(let index = 0;index<imagenes.length;index++){
         const div = document.createElement('div');
         div.classList.add('circles')
@@ -38,7 +34,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
         divIndicadores.appendChild(div);
         
     }
-    progresssBar.style.width = `${porcentaje_base}%`
     img1.src = imagenes[0];
     const circulos = document.querySelectorAll('.circles');
     circulos[0].classList.add('resaltado');
@@ -50,18 +45,18 @@ document.addEventListener('DOMContentLoaded', ()=>{
         circulo_actual.classList.add('resaltado')
         img2.classList.add('active');
         i++;
-        porcentaje_actual += porcentaje_base
-        progresssBar.style.width = `${porcentaje_actual}%`
+        
+        
         if(i==imagenes.length){
             i=0;
-            porcentaje_actual = porcentaje_base - porcentaje_base;
+            
         }
         setTimeout(()=>{
             img1.src = img2.src
             img2.classList.remove('active')
         },1000)
     }
-    setInterval(slideshow,4000)
+    setInterval(slideshow,3000)
 })
 
 // Movie slider
