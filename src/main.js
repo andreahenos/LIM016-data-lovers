ace42fb95712dfb40fbdd94ba2c341f25b98503f
andreahenos <henostroza.andrea@gmail.com>
2021-11-22 10:44:08 -0500

    Se eliminó una función comentada

--- src/main.js
+++ src/main.js
@@ -43,37 +43,6 @@ document.getElementById("btnViewAll").addEventListener("click", ()=>{
     aboutSection.style.display = "none"
 })
 
-/*Cargar Peliculas */
-/* function showDataMain(menuItems) {
-    let displayMovies = menuItems.map((movie)=>{
-
-    return `
-    <article class="movie">
-        <div class="figure">
-            <img src="${movie.poster}" class="img-movie">
-            <div class="capa">
-                <div class="cont-title">
-                <h3>${movie.title}</h3>
-                </div>
-
-                <div class="cont-span">
-                    <p>⭐ ${movie.rt_score}</p>
-                    <p>🎥 ${movie.release_date}</p>
-                </div>
-                <div class="description">
-                <p>${movie.description.split(".", 1)}</p>
-                </div>
-                <a class="btn-mas">See more</a>
-            </div>
-        </div>
-    </article>
-    `
-});
-
-displayMovies = displayMovies.join("");
- return displayMovies
-} */
-
 /*Carrousel Home Arriba */
 document.addEventListener('DOMContentLoaded', ()=>{
 
